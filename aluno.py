765f062f16a957e204991e566d41feadf34319bf
ana clara <aclaramiranda221@gmail.com>
2026-02-20 09:49:41 -0300

    aula do dia vinte do dois

---
+++ aluno.py
@@ -0,0 +1,12 @@
+nome = (input("digite o seu nome"))
+telefone = int(input("digite o seu telefone"))
+idade = int(input("digite o sua idade"))
+cidade = (input("digite sua cidade"))
+
+print("olá", nome)
+print("sua idade é", idade)
+print("seu telefone é", telefone)
+print("sua cidade é", cidade)
+
+
+
