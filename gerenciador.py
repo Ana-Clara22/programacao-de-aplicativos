5ee2c506050161bb7a5de639dc54c0317e4bd15a
ana clara <aclaramiranda221@gmail.com>
2026-03-19 10:04:08 -0300

    19/03

---
+++ gerenciador.py
@@ -0,0 +1,7 @@
+playlist = ["Musica 1", "Muquica 2", "Mulica 3", "Muxica 4", "Muvica 5"]
+print(playlist)
+playlist.remove("Mulica 3")
+print(playlist)
+playlist.pop (0)
+print(playlist)
+print(f"a playlist ficou com {len(playlist)} musicas")
---
+++ verificador.py
@@ -0,0 +1,5 @@
+status = input("digite o seu status")
+vagas = ["Ocupado", "Livre", "Ocupado", "Livre"]
+número = int(input("digite o numero da vaga 0 a 3"))
+if % 2 == 0 and status = = "livre":
+    print(f"Vaga {1} autorizada para estacionar.")
